696fda51db6b70fdae78fab4e407e4428315ead5
Darren Sarra <adriansarra18@gmail.com>
2020-06-10 00:07:01 -0400

    Started a navigation bar.

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Navbar() {
+  return(
+    <nav className="navbar">
+      <span className="navbar-brand">Employee Directory</span>
+    </nav>
+  )
+}
+
+export default Navbar;
